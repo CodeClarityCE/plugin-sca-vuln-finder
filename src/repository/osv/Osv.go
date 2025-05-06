59e6206e3122072bc38f9b27a3ab08e18984a512
CedricHerzog <CedricHerzog@users.noreply.github.com>
2025-05-06 17:29:15 +0200

    Optimize queries to fetch OSV vulns

--- src/repository/osv/Osv.go
+++ src/repository/osv/Osv.go
@@ -7,8 +7,6 @@ import (
 	"github.com/uptrace/bun"
 )
 
-var osvCache map[string][]knowledge_db.OSVItem = map[string][]knowledge_db.OSVItem{}
-
 // GetAllOSVReportsForPurl retrieves all OSV reports for a given package URL (purl).
 // It first checks if the reports are already cached, and if not, it queries the database to fetch the reports.
 // The function returns a slice of OSVItem, which represents the OSV reports, and an error if any.
@@ -20,11 +18,9 @@ func GetAllOSVReportsForPurl(purl string, knowledge *bun.DB) ([]knowledge_db.OSV
 	// TODO avoid SQL injection
 	// ("@purl IN osv_report.affected[*].package.purl")
 	rows, err := knowledge.QueryContext(ctx, `
-		WITH preselect AS(SELECT *, jsonb_path_query("affected", '$[*].package.purl ?(@=="`+purl+`")')
-		FROM osv)
-
 		SELECT DISTINCT "id", "osv_id", "schema_version", "modified", "published", "withdrawn", "aliases", "related", "summary", "details", "severity", "affected", "references", "credits", "database_specific", "cwes", "cve"
-		FROM preselect
+		FROM osv
+		WHERE ("affected" @> '[{"package": {"purl": "`+purl+`"}}]')
 	`)
 	if err != nil {
 		panic(err)
